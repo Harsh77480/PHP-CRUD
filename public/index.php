c8bdcc16acc75df4f9c51388ed5a1a8683ba8db5
Harsh77480 <otherworks77480@gmail.com>
2024-12-15 11:52:18 +0530

    Debugging

--- public/index.php
+++ public/index.php
@@ -16,13 +16,13 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 
 // Add CORS middleware
-$app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-});
+// $app->add(function ($request, $handler) {
+//     $response = $handler->handle($request);
+//     return $response
+//         ->withHeader('Access-Control-Allow-Origin', '*')
+//         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//         ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// });
 
 // Include Routes
 $routes = require __DIR__ . '/../src/routes.php';
